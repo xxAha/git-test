ee631549b4a932c3ce084a2fcb674f07bd276e95
aha <223311795@qq.com>
2020-08-26 08:12:31 +0800

    test

---
+++ test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    test (){
+      console.log(1)
+    },
+    test2 () {
+      console.log(2)
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
