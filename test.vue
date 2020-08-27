80dbe79c5b036663c9e67be1e8f90557827aa6f2
aha <223311795@qq.com>
2020-08-27 22:38:13 +0800

    asdasd

--- test.vue
+++ test.vue
@@ -1,19 +1,30 @@
 <template>
   <div>{{$t('xxxxx')}}
     <h1>111</h1>
+    <h2>{{a}}</h2>
   </div> 
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      a: 222
+    }
+  },
   methods: {
     test (){
+      const b = 20
       console.log('xx222x')
       console.log('9999kl')
-      console.log(1)
+      console.log(b)
     },
     test2 () {
-      console.log(2)
+      const a = 10
+      console.log(a)
+    },
+    test3 (){
+
     }
   },
 }
